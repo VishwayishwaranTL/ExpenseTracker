c504580a0cee83274ad816e9d05969fd937ac006
VishwayishwaranTL <vishwayishwaran2210619@gmail.com>
2025-07-02 17:43:43 +0530

    Image url changes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -16,12 +16,13 @@ router.post("/login",loginUser);
 router.get("/getUser",protect, getUserInfo);
 router.put('/profile',protect, updateProfile);
 
-router.post("/upload-image", upload.single("image"),(req,res) => {
-    if (!req.file){
-        return res.status(400).json({message: "No file uploaded"});
-    }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-    res.status(200).json({imageUrl});
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `/uploads/${req.file.filename}`;
+  res.status(200).json({ imageUrl });
 });
+
 
 export default router;
